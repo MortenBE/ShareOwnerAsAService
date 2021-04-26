1debc026394b82d38285879b750d95e4c0ed856f
MortenBE <Ehlersmorten@gmail.com>
2021-04-26 18:07:56 +0200

    Tilføjet ShareControl
    ShareControl holder styr på share db og kan kaldes med api

---
+++ ShareControl/ShareControl/ShareControl/Data/ShareDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShareControl.Models;
+
+namespace ShareControl.Data
+{
+    public class ShareDbContext : DbContext
+    {
+        public ShareDbContext (DbContextOptions<ShareDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Share> Share { get; set; }
+    }
+}
---
+++ ShareControl/ShareControl/ShareControl/Models/Share.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShareControl.Models
+{
+    public class Share
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int TraderId { get; set; }
+    }
+}
