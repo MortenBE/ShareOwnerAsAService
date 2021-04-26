617036f51dc40aa933e86e9005c4f1d3e823065e
MortenBE <Ehlersmorten@gmail.com>
2021-04-26 20:27:13 +0200

    Tilføjet Trader/owner control. Fjernet ShareOwner projekt

---
+++ TraderControl/TraderControl/TraderControl/Data/TraderDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TraderControl.Models;
+
+namespace TraderControl.Data
+{
+    public class TraderDbContext : DbContext
+    {
+        public TraderDbContext (DbContextOptions<TraderDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TraderControl.Models.Trader> Trader { get; set; }
+    }
+}
