01066d9b9097c3027740f54116643fd3273f3eed
MortenBE <Ehlersmorten@gmail.com>
2021-05-26 19:22:21 +0200

    Template RabbitMQ tilført til TobinTax

--- TobinTaxService/TobinTaxService/Program.cs
+++ TobinTaxService/TobinTaxService/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
 
 namespace TobinTaxService
 {
@@ -13,7 +12,35 @@ namespace TobinTaxService
     {
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                /*
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                */
+            }
             CreateHostBuilder(args).Build().Run();
+
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
