ca018e7ea00e11b814e8d5f9809f1dce23003567
Tobias Holm <201608355@post.au.dk>
2021-05-27 15:32:04 +0200

    asd

---
+++ ShareService/ShareService/Migrations/20210527132606_initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareService.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShareServiceModel",
+                columns: table => new
+                {
+                    ShareId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Stock = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TraderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShareServiceModel", x => x.ShareId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShareServiceModel");
+        }
+    }
+}
