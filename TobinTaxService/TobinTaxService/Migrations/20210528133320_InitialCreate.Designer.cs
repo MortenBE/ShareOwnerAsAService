35d39f62bc2ef0c98fa1510d97f46ea31cf69478
MortenBE <Ehlersmorten@gmail.com>
2021-05-28 15:34:41 +0200

    tobinTax

--- TobinTaxService/TobinTaxService/Migrations/20210528133320_InitialCreate.Designer.cs
+++ TobinTaxService/TobinTaxService/Migrations/20210528133320_InitialCreate.Designer.cs
@@ -8,8 +8,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace TobinTaxService.Migrations
 {
     [DbContext(typeof(TobinTaxDbContext))]
-    [Migration("20210528085043_initMigration")]
-    partial class initMigration
+    [Migration("20210528133320_InitialCreate")]
+    partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
--- TobinTaxService/TobinTaxService/Migrations/20210528133320_InitialCreate.cs
+++ TobinTaxService/TobinTaxService/Migrations/20210528133320_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace TobinTaxService.Migrations
 {
-    public partial class initMigration : Migration
+    public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
